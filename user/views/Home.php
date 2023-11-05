9dc86f320779bdecac5a500fdc6867975051877c
0359689640Diep <134345504+0359689640Diep@users.noreply.github.com>
2023-11-05 12:11:53 +0700

    Add home fontend

---
+++ user/views/Home.php
@@ -0,0 +1,294 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<link rel="stylesheet" href="../../assets/css/user/Home.css">
+<link rel="stylesheet" href="../../assets/themify-icons/themify-icons.css">
+
+<body>
+<section class="page">
+        <header>               
+        </header>
+        <main>
+            <section class="seoProduct">
+                <section class="containerItem1">
+                    <section class="contentItem1">
+                        <h1>Ăn uống lành mạnh là quan trọng một phần của lối sống</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque congue arcu</p>
+                    </section>
+                    <section class="contentItemImg">
+                        <img src="../../assets/img/Image.png" alt="img">
+                        <article class="lisstImg">
+                            <img src="../../assets/img/spices1.png" alt="img">
+                            <img src="../../assets/img/spices2.png" alt="img">
+                            <img src="../../assets/img/spices3.png" alt="img">
+                        </article>
+                    </section>
+                </section>
+                <section class="containerItem2">
+                    <section class="product">
+                        <img src="../../assets/img/Image1.png" alt="img">
+                        <h1>Bắt đầu lên kế hoạch ăn kiêng ngay hôm nay</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque congue arcu</p>
+                    </section>
+                    <section class="product">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque congue arcu</p>
+                        <img src="../../assets/img/Image2.png" alt="img">
+                    </section>
+                </section>
+            </section>
+            <section class="myProduct">
+                <section class="contentMyProduct">
+                    <h1>Sản phẩm của chúng tôi</h1>
+                </section>
+                <section class="listProduct">
+                    
+                            <section class="contentListProduct">
+                                <article class="image">
+                                <img src="../../assets/img/Image3.png" alt="img">
+                                </article>
+                                <article class="title">
+                                    <h1>Bít tết với kim chi nướng</h1>
+                                </article>
+                                <article class="description">
+                                    <p>Thị bò được nhập khẩu từ mĩ. Với các cô bò hành phúc, đem lại trải nhiệm tuyệt vời</p>
+                                </article>
+                                <article class="price">
+                                    <h1>145$</h1>
+                                </article>
+        
+                                <article class="buttonAction">
+                                    <button>Thêm vào giỏ hàng</button>
+                                    <button>Mua ngay</button>
+                                </article>
+                            </section>                            
+                            
+                            <section class="contentListProduct">
+                                <article class="image">
+                                <img src="../../assets/img/Image3.png" alt="img">
+                                </article>
+                                <article class="title">
+                                    <h1>Bít tết với kim chi nướng</h1>
+                                </article>
+                                <article class="description">
+                                    <p>Thị bò được nhập khẩu từ mĩ. Với các cô bò hành phúc, đem lại trải nhiệm tuyệt vời</p>
+                                </article>
+                                <article class="price">
+                                    <h1>145$</h1>
+                                </article>
+        
+                                <article class="buttonAction">
+                                    <button>Thêm vào giỏ hàng</button>
+                                    <button>Mua ngay</button>
+                                </article>
+                            </section>                            
+                            
+                            <section class="contentListProduct">
+                                <article class="image">
+                                <img src="../../assets/img/Image3.png" alt="img">
+                                </article>
+                                <article class="title">
+                                    <h1>Bít tết với kim chi nướng</h1>
+                                </article>
+                                <article class="description">
+                                    <p>Thị bò được nhập khẩu từ mĩ. Với các cô bò hành phúc, đem lại trải nhiệm tuyệt vời</p>
+                                </article>
+                                <article class="price">
+                                    <h1>145$</h1>
+                                </article>
+        
+                                <article class="buttonAction">
+                                    <button>Thêm vào giỏ hàng</button>
+                                    <button>Mua ngay</button>
+                                </article>
+                            </section>                            
+                            
+                            <section class="contentListProduct">
+                                <article class="image">
+                                <img src="../../assets/img/Image3.png" alt="img">
+                                </article>
+                                <article class="title">
+                                    <h1>Bít tết với kim chi nướng</h1>
+                                </article>
+                                <article class="description">
+                                    <p>Thị bò được nhập khẩu từ mĩ. Với các cô bò hành phúc, đem lại trải nhiệm tuyệt vời</p>
+                                </article>
+                                <article class="price">
+                                    <h1>145$</h1>
+                                </article>
+        
+                                <article class="buttonAction">
+                                    <button>Thêm vào giỏ hàng</button>
+                                    <button>Mua ngay</button>
+                                </article>
+                            </section>                            
+                            
+                            <section class="contentListProduct">
+                                <article class="image">
+                                <img src="../../assets/img/Image3.png" alt="img">
+                                </article>
+                                <article class="title">
+                                    <h1>Bít tết với kim chi nướng</h1>
+                                </article>
+                                <article class="description">
+                                    <p>Thị bò được nhập khẩu từ mĩ. Với các cô bò hành phúc, đem lại trải nhiệm tuyệt vời</p>
+                                </article>
+                                <article class="price">
+                                    <h1>145$</h1>
+                                </article>
+        
+                                <article class="buttonAction">
+                                    <button>Thêm vào giỏ hàng</button>
+                                    <button>Mua ngay</button>
+                                </article>
+                            </section>                            
+                                            </section>
+            </section>
+            <section class="chef">
+                <img src="../../assets/img/Image4.png" alt="img">
+                <img src="../../assets/img/Leaf1.png" alt="img">
+                <img src="../../assets/img/Leaf.png" alt="img">
+                <article class="contentChef">
+                    <h1>Đầu bếp xuất sắc</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus lorem id penatibus imperdiet. Turpis egestas ultricies purus auctor tincidunt lacus nunc. </p>
+                </article>
+            
+            </section>
+            <section class="seoIngredient">
+                <section class="Ingredient">
+                <img src="../../assets/img/CircleIcon.png" alt="img">
+                    <h1>Chất lượng cao</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque congue arcu</p>
+                </section>
+                <section class="Ingredient">
+                <img src="../../assets/img/CircleIcon1.png" alt="img">
+                    <h1>Rau theo mùa</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque congue arcu</p>
+                </section>
+                <section class="Ingredient">
+                <img src="../../assets/img/CircleIcon2.png" alt="img">
+                    <h1>Hoa quả tươi</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque congue arcu</p>
+                </section>
+                    
+            </section>
+            <section class="bestProducts">
+                <article class="contentTitileBestProduct">
+                    <h1>Các sản phẩm bán chạy nhất</h1>
+                </article>
+                <i class="ti-arrow-left"></i>
+                <i class="ti-arrow-right"></i>
+                <section class="contentLisstBestProducts">
+                    <section class="lisstBestProducts">
+                        <img src="../../assets/img/FeatureImage.png" alt="img">
+                            <article class="titile">
+                                <h1>Thị bò mỹ cao cấp</h1>
+                            </article>
+                            <article class="description">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                            </article>
+
+                    </section>
+                    <section class="lisstBestProducts">
+                         <img src="../../assets/img/FeatureImage1.png" alt="img">
+                        <article class="titile">
+                            <h1>Thị bò hun khói </h1>
+                        </article>
+                        <article class="description">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                        </article>
+
+                    </section>
+                    <section class="lisstBestProducts">
+                        <img src="" alt="">
+                        <article class="titile">
+                            <h1></h1>
+                            <article class="description">
+                                <p></p>
+                            </article>
+                        </article>
+
+                    </section>
+                </section>
+            </section>
+            <section class="book">
+                <section class="contentBook">
+                    <article class="headerBook">
+                        <h1>Đặt bàn</h1>
+                        <p>Liên hệ với nhà hàng</p>
+                    </article>
+                    <form action="" method="post">
+                        <section class="contentForm">
+                            <input type="date" name="" id="">
+                            <select name="" id="" class="time">
+                                <option value="">Time</option>
+                            </select>
+                            <select name="" id="" class="persion">
+                                <option value="">Time</option>
+    
+                            </select>
+                        </section>
+                        <article class="footerBook">
+                            <button>Đặt Ngay</button>
+                        </article>
+                    </form>
+                </section>
+            </section>
+            <section class="calorieBalance">
+                <article class="contentCalorieBalance">
+                    <h1>Cân bằng năng lượng calo</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </article>
+                <i class="ti-arrow-left"></i>
+                <i class="ti-arrow-right"></i>
+                <section class="productCalorieBalance">
+                    <article class="contentProductCalorieBalance">
+                        <img src="../../assets/img/Image5.png" alt="img">
+                        <h1>Starters</h1>
+                    </article>
+                    <article class="contentProductCalorieBalance">
+                        <img src="../../assets/img/MenuCategory.png" alt="img">
+                        <h1>Mains</h1>
+                    </article>
+                    <article class="contentProductCalorieBalance">
+                        <img src="../../assets/img/Image6.png" alt="img">
+                        <h1>Soups</h1>
+                    </article>
+                    <article class="contentProductCalorieBalance">
+                        <img src="../../assets/img/Image6.png" alt="img">
+                        <h1>Soups</h1>
+                    </article>
+                
+                </section>
+            </section>
+            <section class="displayCommet">
+                <article class="contentDispayComment">
+                    <h1> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus lorem id penatibus imperdiet. Turpis egestas ultricies purus  Lorem ipsum dolor sit amet.</h1>
+                </article>
+                <section class="itemDispayComment">
+                    <section class="persion">
+                        <img src="../../assets/img/Avatar.svg" alt="img">
+                        <article class="title">
+                            <h1>John Doe</h1>
+                            <p>Bloger</p>
+                        </article>
+                    </section>
+                    <nav class="Nav">
+                    <i class="ti-arrow-left"></i>
+                        <article class="contentNav">
+                            <h3>2</h3>
+                            <h3>/</h3>
+                            <h3>3</h3>
+                        </article>
+                    <i class="ti-arrow-right"></i>
+                    </nav>
+                </section>
+            </section>
+        </main>
+        <footer></footer>
+    </section>
+</body>
+</html>
