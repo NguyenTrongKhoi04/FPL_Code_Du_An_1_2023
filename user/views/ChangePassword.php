d186aeb9002be1495d7d2af1b0aecca093b0942d
0359689640Diep <134345504+0359689640Diep@users.noreply.github.com>
2023-11-06 08:43:48 +0700

    add ChagePassword fontend

---
+++ user/views/ChangePassword.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<link rel="stylesheet" href="../../assets/css/user/ChangePassword.css">
+<link rel="stylesheet" href="../../assets/themify-icons/themify-icons.css">
+<body>
+    <section class="page">
+        <article class="banner">
+            <img src="../../assets/img/banner1.png" alt="banner1">
+        </article>
+        <section class="main">
+            <article class="headerMain">
+                <h1>Đăng nhập </h1>
+            </article>
+            
+            <form action="" method="post" class="main">
+                <label for="">Tài khoản *</label>
+                <input type="text"  name = "account">
+                <label for="">Mật khẩu *</label>
+                <input  type = "password" name = "password">
+                <input  type = "submit" name="submit" value="Đăng nhập">
+                <a href="#">Quên mật khẩu ?</a>
+            </form>
+            <section class="footerMain">
+                <section class="itemContent1">
+                    <article></article>
+                    <article>Hoặc</article>
+                    <article></article>
+                </section>
+                <section class="itemContent2">
+                    <p>Đăng ký</p>
+                    <p>nếu bạn chưa có tải khoản ?</p>
+                </section>
+            </section>
+        </section>
+    </section>
+</body>
+</html>
